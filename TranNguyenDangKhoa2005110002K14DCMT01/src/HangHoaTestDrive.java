ec73527921fc9a91ff12a6c40f3ef5fec896ad7a
TNDangKhoa <khoabo120822@gmail.com>
2022-04-21 11:19:47 +0700

    Hoanh thanh

--- TranNguyenDangKhoa2005110002K14DCMT01/src/HangHoaTestDrive.java
+++ TranNguyenDangKhoa2005110002K14DCMT01/src/HangHoaTestDrive.java
@@ -6,12 +6,13 @@ import java.util.Scanner;
 public class HangHoaTestDrive {
     public static void main(String[] args) throws ParseException {
         System.out.println("CHUONG TRINH QUAN LI HANG HOA");
-		System.out.println(">>>>>>>>>>>>>MENU<<<<<<<<<<<<<");
+		System.out.println("=============FUNCTION MENU===============");
         System.out.println("1. Them hang hoa");
         System.out.println("2. Tim kiem");
         System.out.println("3. Show danh sach");
         System.out.println("4. Sua thong tin hang hoa");
         System.out.println("5. Xoa hang hoa");
+        System.out.println("=========================================");
 
         DSHangHoa dsHangHoa = new DSHangHoa();
         Scanner nhap = InputSupport.nhap;
@@ -45,123 +46,129 @@ public class HangHoaTestDrive {
         System.out.println("Chon tinh nang(1-5): ");
         choice = nhap.nextInt();
         nhap.nextLine();
-        switch (choice){
-            case 1: 
-            do {
+        if (choice >= 1 && choice <= 5){
+            switch (choice){
+                case 1: 
+                do {
+                    System.out.print("Nhap ma hang: ");
+                    String mahang = nhap.nextLine();
+                    System.out.print("Nhap ten hang hoa: ");
+                    String tenhanghoa = nhap.nextLine();
+                    System.out.print("Nhap so luong: ");
+                    Double soluong = nhap.nextDouble();
+                    nhap.nextLine();
+                    System.out.print("Nhap don gia: ");
+                    double dongia = nhap.nextDouble();
+                    System.out.print("Nhap loai hang(1 - Hang thuc pham, 2 - Hang sanh su, 3 - Hang dien may): ");
+                    int loai = nhap.nextInt();
+                    nhap.nextLine();
+        
+                    if (loai == 1) {
+                        System.out.print("Nhap ngay san xuat");
+                        Date nSX = date.parse(nhap.nextLine());
+                        System.out.print("Nhap han su dung");
+                        Date hSD = date.parse(nhap.nextLine());
+                        HangThucPham hangTP = new HangThucPham(mahang, tenhanghoa, soluong, dongia, nSX, hSD);
+                        dsHangHoa.them(hangTP);
+                        dsHangHoa.inDS();
+                    }else{
+                        if (loai == 2) {
+                            System.out.println("Nhap ngay nhap kho: ");
+                            Date ngayNhapKho = date.parse(nhap.nextLine());
+                            System.out.println("Nhap nha san xuat: ");
+                            String nhaSX = nhap.nextLine();
+                            HangSanhSu hangSanhSu = new HangSanhSu(mahang, tenhanghoa, soluong, dongia, ngayNhapKho, nhaSX);
+                            dsHangHoa.them(hangSanhSu);
+                            dsHangHoa.inDS();
+                        }else{
+                            if (loai == 3){
+                                System.out.println("Nhap cong suat: ");
+                                Double congsuat = nhap.nextDouble();
+                                System.out.println("Nhap Bao hanh: ");
+                                int baohanh = nhap.nextInt();
+                                nhap.nextLine();
+                                HangDienMay hangDienMay = new HangDienMay(mahang, tenhanghoa, soluong, dongia, congsuat, baohanh);
+                                dsHangHoa.them(hangDienMay);
+                                dsHangHoa.inDS();
+                            }
+                        }
+                    }
+                    System.out.print("Ban co muon nhap nua khong Y/N>>");
+                    nhapTiep = nhap.nextLine();
+                } while (nhapTiep.equals("Y"));
+                break;
+                case 2:
+                System.out.println("Nhap ma hang can tim: ");
+                String maHang = nhap.nextLine();
+                HangHoa SearchHangHoa = dsHangHoa.timKiemHangHoaTheoMa(maHang);
+                System.out.println(SearchHangHoa);
+                break;
+                case 3:
+                dsHangHoa.inDS();
+                break;
+                case 4:
+                System.out.println("Nhap ma hang can sua: ");
+                String maHangThem = nhap.nextLine();
+                HangHoa suaHangHoa = dsHangHoa.timKiemHangHoaTheoMa(maHangThem);
+                dsHangHoa.xoa(suaHangHoa);
+    
                 System.out.print("Nhap ma hang: ");
-                String mahang = nhap.nextLine();
-                System.out.print("Nhap ten hang hoa: ");
-                String tenhanghoa = nhap.nextLine();
-                System.out.print("Nhap so luong: ");
-                Double soluong = nhap.nextDouble();
-                nhap.nextLine();
-                System.out.print("Nhap don gia: ");
-                double dongia = nhap.nextDouble();
-                System.out.print("Nhap loai hang(1 - Hang thuc pham, 2 - Hang sanh su, 3 - Hang dien may): ");
-                int loai = nhap.nextInt();
-                nhap.nextLine();
-    
-                if (loai == 1) {
-                    System.out.print("Nhap ngay san xuat");
-                    Date nSX = date.parse(nhap.nextLine());
-                    System.out.print("Nhap han su dung");
-                    Date hSD = date.parse(nhap.nextLine());
-                    HangThucPham hangTP = new HangThucPham(mahang, tenhanghoa, soluong, dongia, nSX, hSD);
-                    dsHangHoa.them(hangTP);
-                    dsHangHoa.inDS();
-                }else{
-                    if (loai == 2) {
-                        System.out.println("Nhap ngay nhap kho: ");
-                        Date ngayNhapKho = date.parse(nhap.nextLine());
-                        System.out.println("Nhap nha san xuat: ");
-                        String nhaSX = nhap.nextLine();
-                        HangSanhSu hangSanhSu = new HangSanhSu(mahang, tenhanghoa, soluong, dongia, ngayNhapKho, nhaSX);
-                        dsHangHoa.them(hangSanhSu);
+                    String mahang = nhap.nextLine();
+                    System.out.print("Nhap ten hang hoa: ");
+                    String tenhanghoa = nhap.nextLine();
+                    System.out.print("Nhap so luong: ");
+                    Double soluong = nhap.nextDouble();
+                    nhap.nextLine();
+                    System.out.print("Nhap don gia: ");
+                    double dongia = nhap.nextDouble();
+                    System.out.print("Nhap loai hang(1 - Hang thuc pham, 2 - Hang sanh su, 3 - Hang dien may): ");
+                    int loai = nhap.nextInt();
+                    nhap.nextLine();
+        
+                    if (loai == 1) {
+                        System.out.print("Nhap ngay san xuat");
+                        Date nSX = date.parse(nhap.nextLine());
+                        System.out.print("Nhap han su dung");
+                        Date hSD = date.parse(nhap.nextLine());
+                        HangThucPham hangTP = new HangThucPham(mahang, tenhanghoa, soluong, dongia, nSX, hSD);
+                        dsHangHoa.them(hangTP);
                         dsHangHoa.inDS();
                     }else{
-                        if (loai == 3){
-                            System.out.println("Nhap cong suat: ");
-                            Double congsuat = nhap.nextDouble();
-                            System.out.println("Nhap Bao hanh: ");
-                            int baohanh = nhap.nextInt();
-                            nhap.nextLine();
-                            HangDienMay hangDienMay = new HangDienMay(mahang, tenhanghoa, soluong, dongia, congsuat, baohanh);
-                            dsHangHoa.them(hangDienMay);
+                        if (loai == 2) {
+                            System.out.println("Nhap ngay nhap kho: ");
+                            Date ngayNhapKho = date.parse(nhap.nextLine());
+                            System.out.println("Nhap nha san xuat: ");
+                            String nhaSX = nhap.nextLine();
+                            HangSanhSu hangSanhSu = new HangSanhSu(mahang, tenhanghoa, soluong, dongia, ngayNhapKho, nhaSX);
+                            dsHangHoa.them(hangSanhSu);
                             dsHangHoa.inDS();
+                        }else{
+                            if (loai == 3){
+                                System.out.println("Nhap cong suat: ");
+                                Double congsuat = nhap.nextDouble();
+                                System.out.println("Nhap Bao hanh: ");
+                                int baohanh = nhap.nextInt();
+                                nhap.nextLine();
+                                HangDienMay hangDienMay = new HangDienMay(mahang, tenhanghoa, soluong, dongia, congsuat, baohanh);
+                                dsHangHoa.them(hangDienMay);
+                                dsHangHoa.inDS();
+                            }
                         }
                     }
-                }
-                System.out.print("Ban co muon nhap nua khong Y/N>>");
-                nhapTiep = nhap.nextLine();
-            } while (nhapTiep.equals("Y"));
-            break;
-            case 2:
-            System.out.println("Nhap ma hang can tim: ");
-            String maHang = nhap.nextLine();
-            HangHoa SearchHangHoa = dsHangHoa.timKiemHangHoaTheoMa(maHang);
-            System.out.println(SearchHangHoa);
-            break;
-            case 3:
-            dsHangHoa.inDS();
-            break;
-            case 4:
-            System.out.println("Nhap ma hang can sua: ");
-            String maHangThem = nhap.nextLine();
-            HangHoa suaHangHoa = dsHangHoa.timKiemHangHoaTheoMa(maHangThem);
-            dsHangHoa.xoa(suaHangHoa);
-
-            System.out.print("Nhap ma hang: ");
-                String mahang = nhap.nextLine();
-                System.out.print("Nhap ten hang hoa: ");
-                String tenhanghoa = nhap.nextLine();
-                System.out.print("Nhap so luong: ");
-                Double soluong = nhap.nextDouble();
-                nhap.nextLine();
-                System.out.print("Nhap don gia: ");
-                double dongia = nhap.nextDouble();
-                System.out.print("Nhap loai hang(1 - Hang thuc pham, 2 - Hang sanh su, 3 - Hang dien may): ");
-                int loai = nhap.nextInt();
-                nhap.nextLine();
+                break;
+                case 5:
+                System.out.println("Nhap ma hang can xoa: ");
+                String maHangXoa = nhap.nextLine();
+                HangHoa delete = dsHangHoa.timKiemHangHoaTheoMa(maHangXoa);
+                dsHangHoa.xoa(delete);
+                dsHangHoa.inDS();
+                break;
     
-                if (loai == 1) {
-                    System.out.print("Nhap ngay san xuat");
-                    Date nSX = date.parse(nhap.nextLine());
-                    System.out.print("Nhap han su dung");
-                    Date hSD = date.parse(nhap.nextLine());
-                    HangThucPham hangTP = new HangThucPham(mahang, tenhanghoa, soluong, dongia, nSX, hSD);
-                    dsHangHoa.them(hangTP);
-                    dsHangHoa.inDS();
-                }else{
-                    if (loai == 2) {
-                        System.out.println("Nhap ngay nhap kho: ");
-                        Date ngayNhapKho = date.parse(nhap.nextLine());
-                        System.out.println("Nhap nha san xuat: ");
-                        String nhaSX = nhap.nextLine();
-                        HangSanhSu hangSanhSu = new HangSanhSu(mahang, tenhanghoa, soluong, dongia, ngayNhapKho, nhaSX);
-                        dsHangHoa.them(hangSanhSu);
-                        dsHangHoa.inDS();
-                    }else{
-                        if (loai == 3){
-                            System.out.println("Nhap cong suat: ");
-                            Double congsuat = nhap.nextDouble();
-                            System.out.println("Nhap Bao hanh: ");
-                            int baohanh = nhap.nextInt();
-                            nhap.nextLine();
-                            HangDienMay hangDienMay = new HangDienMay(mahang, tenhanghoa, soluong, dongia, congsuat, baohanh);
-                            dsHangHoa.them(hangDienMay);
-                            dsHangHoa.inDS();
-                        }
-                    }
-                }
-            break;
-            case 5:
-            System.out.println("Nhap ma hang can xoa: ");
-            String maHangXoa = nhap.nextLine();
-            HangHoa delete = dsHangHoa.timKiemHangHoaTheoMa(maHangXoa);
-            dsHangHoa.xoa(delete);
-            dsHangHoa.inDS();
-            break;
-
+            }
+        }else{
+            System.out.println("Khong co chuc nang nay!");
         }
+        
     }
 }
+
