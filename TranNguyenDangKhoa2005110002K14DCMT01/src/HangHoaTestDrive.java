0416b79e322ec24ff6f6f0baab5eaa3bce59f476
TNDangKhoa <khoabo120822@gmail.com>
2022-04-21 11:12:29 +0700

    Hoan thanh

--- TranNguyenDangKhoa2005110002K14DCMT01/src/HangHoaTestDrive.java
+++ TranNguyenDangKhoa2005110002K14DCMT01/src/HangHoaTestDrive.java
@@ -19,8 +19,30 @@ public class HangHoaTestDrive {
 
         SimpleDateFormat date = new SimpleDateFormat("dd/MM/yyyy");
 
+        //dl cho san
+        HangHoa sp1 = new HangThucPham("TP01", "Trung", 12, 10_000, date.parse("12/02/2012"), date.parse("21/05/2012"));
+        HangHoa sp2 = new HangThucPham("TP02", "Ca rot", 3, 11_000, date.parse("12/03/2012"), date.parse("10/05/2012"));
+        HangHoa sp3 = new HangThucPham("TP03", "Khoai tay", 2, 12_000, date.parse("12/01/2012"), date.parse("13/05/2012"));
+        HangHoa sp4 = new HangDienMay("DM01", "May giat", 2, 11_000_000, 1200, 3);
+        HangHoa sp5 = new HangDienMay("DM02", "May lanh", 5, 15_000_000, 2200, 3);
+        HangHoa sp6 = new HangDienMay("DM03", "Tu lanh", 3, 10_000_000, 800, 3);
+        HangHoa sp7 = new HangSanhSu("SS01", "Binh su", 13, 3_000_000, date.parse("25/12/2015"), "Cong ty abc");
+        HangHoa sp8 = new HangSanhSu("SS02", "Chen su", 10, 1_000_000, date.parse("25/12/2015"), "Cong ty abc");
+        HangHoa sp9 = new HangSanhSu("SS03", "Dia su", 8, 1_000_000, date.parse("25/12/2015"), "Cong ty abc");
+        HangHoa sp10 = new HangSanhSu("SS04", "Am su", 5, 2_000_000, date.parse("25/12/2015"), "Cong ty abc");
+
+        dsHangHoa.them(sp1);
+        dsHangHoa.them(sp2);
+        dsHangHoa.them(sp3);
+        dsHangHoa.them(sp4);
+        dsHangHoa.them(sp5);
+        dsHangHoa.them(sp6);
+        dsHangHoa.them(sp7);
+        dsHangHoa.them(sp8);
+        dsHangHoa.them(sp9);
+        dsHangHoa.them(sp10);
         int choice;
-        System.out.println("Chon tinh nang(1-3): ");
+        System.out.println("Chon tinh nang(1-5): ");
         choice = nhap.nextInt();
         nhap.nextLine();
         switch (choice){
@@ -46,6 +68,7 @@ public class HangHoaTestDrive {
                     Date hSD = date.parse(nhap.nextLine());
                     HangThucPham hangTP = new HangThucPham(mahang, tenhanghoa, soluong, dongia, nSX, hSD);
                     dsHangHoa.them(hangTP);
+                    dsHangHoa.inDS();
                 }else{
                     if (loai == 2) {
                         System.out.println("Nhap ngay nhap kho: ");
@@ -54,6 +77,7 @@ public class HangHoaTestDrive {
                         String nhaSX = nhap.nextLine();
                         HangSanhSu hangSanhSu = new HangSanhSu(mahang, tenhanghoa, soluong, dongia, ngayNhapKho, nhaSX);
                         dsHangHoa.them(hangSanhSu);
+                        dsHangHoa.inDS();
                     }else{
                         if (loai == 3){
                             System.out.println("Nhap cong suat: ");
@@ -63,10 +87,11 @@ public class HangHoaTestDrive {
                             nhap.nextLine();
                             HangDienMay hangDienMay = new HangDienMay(mahang, tenhanghoa, soluong, dongia, congsuat, baohanh);
                             dsHangHoa.them(hangDienMay);
+                            dsHangHoa.inDS();
                         }
                     }
                 }
-                System.out.print("May co muon nhap nua khong Y/N>>");
+                System.out.print("Ban co muon nhap nua khong Y/N>>");
                 nhapTiep = nhap.nextLine();
             } while (nhapTiep.equals("Y"));
             break;
@@ -105,6 +130,7 @@ public class HangHoaTestDrive {
                     Date hSD = date.parse(nhap.nextLine());
                     HangThucPham hangTP = new HangThucPham(mahang, tenhanghoa, soluong, dongia, nSX, hSD);
                     dsHangHoa.them(hangTP);
+                    dsHangHoa.inDS();
                 }else{
                     if (loai == 2) {
                         System.out.println("Nhap ngay nhap kho: ");
@@ -113,6 +139,7 @@ public class HangHoaTestDrive {
                         String nhaSX = nhap.nextLine();
                         HangSanhSu hangSanhSu = new HangSanhSu(mahang, tenhanghoa, soluong, dongia, ngayNhapKho, nhaSX);
                         dsHangHoa.them(hangSanhSu);
+                        dsHangHoa.inDS();
                     }else{
                         if (loai == 3){
                             System.out.println("Nhap cong suat: ");
@@ -122,6 +149,7 @@ public class HangHoaTestDrive {
                             nhap.nextLine();
                             HangDienMay hangDienMay = new HangDienMay(mahang, tenhanghoa, soluong, dongia, congsuat, baohanh);
                             dsHangHoa.them(hangDienMay);
+                            dsHangHoa.inDS();
                         }
                     }
                 }
@@ -131,6 +159,7 @@ public class HangHoaTestDrive {
             String maHangXoa = nhap.nextLine();
             HangHoa delete = dsHangHoa.timKiemHangHoaTheoMa(maHangXoa);
             dsHangHoa.xoa(delete);
+            dsHangHoa.inDS();
             break;
 
         }
