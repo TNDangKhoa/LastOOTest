c29074b203c6adc917585cc8a5be55be2aa09a15
TNDangKhoa <khoabo120822@gmail.com>
2022-04-21 11:20:56 +0700

    1 chut chinh sua

--- TranNguyenDangKhoa2005110002K14DCMT01/src/DSHangHoa.java
+++ TranNguyenDangKhoa2005110002K14DCMT01/src/DSHangHoa.java
@@ -4,21 +4,6 @@ import java.util.ArrayList;
 public class DSHangHoa {
     private List<HangHoa> danhSach = new ArrayList<>();
 
-    public static ArrayList<HangHoa> danhSachHangHoa(){
-        ArrayList<HangHoa> dsHangHoa = new ArrayList<HangHoa>();
-        dsHangHoa.add(new HangHoa("TP01", "Trung", 12, 10000));
-        dsHangHoa.add(new HangHoa("TP02", "ca rot", 4, 12000));
-        dsHangHoa.add(new HangHoa("TP03", "khoai tay", 2, 9000));
-        dsHangHoa.add(new HangHoa("DM01", "Tu lanh", 3, 15000000));
-        dsHangHoa.add(new HangHoa("DM02", "May giat", 4, 1000000));
-        dsHangHoa.add(new HangHoa("DM03", "May lanh", 3, 18000000));
-        dsHangHoa.add(new HangHoa("SS01", "Chen su", 20, 10000));
-        dsHangHoa.add(new HangHoa("SS02", "Muong su", 17, 10000));
-        dsHangHoa.add(new HangHoa("SS03", "Dia su", 21, 10000));
-        dsHangHoa.add(new HangHoa("SS04", "Binh su", 25, 10000));
-        return dsHangHoa;
-    }
-
     public void them(HangHoa hangHoa){
         danhSach.add(hangHoa);
     }
